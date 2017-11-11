91474866c7484b1fe35795d098fbf4ca7d144fac
Jordi Goyanes Granados <jordigoyanes@gmail.com>
2017-11-11 17:16:01 +0100

    Update database.js

--- BitBlocks/database.js
+++ BitBlocks/database.js
@@ -1,5 +1,3 @@
-var alldata = scload('serverdb.json');
-var configFile = scload('bitblocks-config.json')
 var getNewAccount = require('./rtwireAPI.js').getNewAccount;
 var getNewBtcAddress = require('./rtwireAPI.js').getNewBtcAddress;
 var updateBalance = require('./rtwireAPI.js').updateBalance;
@@ -11,31 +9,36 @@ If not, it requests a new Account ID and Bitcoin address from RTWire and stores 
 Then it shows his scoreboard.
 */
 var playerJoin = function(event){
+  var player = event.getPlayer();
+  var playerUUID = player.uniqueId;
+  var alldata = scload('serverdb.json');
+
   if(alldata === null) {
-      alldata = {};
-      alldata.chunks = {};
-      alldata.wallets = {};
-      scsave(alldata, 'serverdb.json');
-}
-var player = event.getPlayer();
-var playerUUID = player.uniqueId;
-if(alldata.wallets[playerUUID] == null){
-  var newAccID = getNewAccount();
-  var btcAddress = getNewBtcAddress(newAccID);
-  alldata.wallets[playerUUID] = {};
-  alldata.wallets[playerUUID].AccountID = newAccID;
-  alldata.wallets[playerUUID].BTCaddress = btcAddress;
-  scsave(alldata, 'serverdb.json');
-  WelcomeMsg(player, alldata.wallets[playerUUID].BTCaddress);
-  var newBalance = updateBalance(player, alldata)
-  updateScoreboard(player, newBalance)
+        print("Bitblocks: Creating database(serverdb.json) to store player data...")
+        alldata = {};
+        alldata.chunks = {};
+        alldata.wallets = {};
+        scsave(alldata, 'serverdb.json');
+    }
 
-}else{
-  var newBalance = updateBalance(player)
-  WelcomeMsg(player, alldata.wallets[playerUUID].BTCaddress);
-  updateScoreboard(player, newBalance)
-}
+  if(alldata.wallets[playerUUID] == null){
+    print("Bitblocks: Creating account id and bitcoin address for new player...")
+    var newAccID = getNewAccount();
+    var btcAddress = getNewBtcAddress(newAccID);
+    alldata.wallets[playerUUID] = {};
+    alldata.wallets[playerUUID].AccountID = newAccID;
+    alldata.wallets[playerUUID].BTCaddress = btcAddress;
+    scsave(alldata, 'serverdb.json');
+    WelcomeMsg(player, alldata.wallets[playerUUID].BTCaddress);
+    var newBalance = updateBalance(player, alldata)
+    updateScoreboard(player, newBalance)
 
+  }else{
+    print("Bitblocks: Loading existing wallet from the database(serverdb.json)...")
+    var newBalance = updateBalance(player)
+    WelcomeMsg(player, alldata.wallets[playerUUID].BTCaddress);
+    updateScoreboard(player, newBalance)
+  }
 }
 
 events.playerJoin(playerJoin);
@@ -49,11 +52,15 @@ var WelcomeMsg = function(player, btcAddress){
 
 
 commando('wallet', function(args, player) {
-		var playerUUID = player.uniqueId;
-		var btcAddress = alldata.wallets[playerUUID].BTCaddress;
-		echo(player, "Deposit Bitcoin Address: https://blockchain.info/address/" + btcAddress);
-		// echo(player, "Official Website: https://bitblocks.netlify.com/");
-		var newBalance = updateBalance(player);
-		echo(player, "Balance: ".bold().gold() + newBalance.toString());
-    		updateScoreboard(player, newBalance);
+  var alldata = scload('serverdb.json');
+	var playerUUID = player.uniqueId;
+	var btcAddress = alldata.wallets[playerUUID].BTCaddress;
+  echo(player, "WALLET INFO".bold().green())
+  echo(player, "------------------------------------------------".green())
+	echo(player, "Deposit Bitcoin Address: https://blockchain.info/address/".aqua() + btcAddress);
+	// echo(player, "Official Website: https://bitblocks.netlify.com/");
+	var newBalance = updateBalance(player);
+	echo(player, "Balance: ".bold().gold() + newBalance.toString());
+  updateScoreboard(player, newBalance);
+  echo(player, "Use /bithelp for more info".gray())
 });
