7f65803907caf06864b8dbad6ca1eaf761126333
Jordi Goyanes Granados <jordigoyanes@gmail.com>
2017-12-23 18:11:17 +0100

    Update scoreboard.js

--- BitBlocks/scoreboard.js
+++ BitBlocks/scoreboard.js
@@ -1,17 +1,20 @@
-var commando = require('../../commando/commando.js').commando
+var commando = require('../../commando/commando.js').commando;
 var Bukkit = org.bukkit.Bukkit;
 var manager = Bukkit.getScoreboardManager();
-var DisplaySlot = org.bukkit.scoreboard.DisplaySlot
-
+var DisplaySlot = org.bukkit.scoreboard.DisplaySlot;
 
 exports.updateScoreboard = function(player, newScore){
-  var board = manager.getNewScoreboard();
-  var objective = board.registerNewObjective("test", "dummy");
-  var score = objective.getScore("Bits: ".gray()); //Get a fake offline player
-  score.setScore(newScore);
-  //Setting where to display the scoreboard/objective (either SIDEBAR, PLAYER_LIST or BELOW_NAME)
-  objective.setDisplaySlot(DisplaySlot.SIDEBAR);
-  //Setting the display name of the scoreboard/objective
-  objective.setDisplayName("Bitcoin ".gold()+"Wallet".gray());
-  player.setScoreboard(board);
+  var config = scload("bitblocks-config.json");
+  var isScoreboardEnabled = config.showScoreboard;
+  if(isScoreboardEnabled === true){
+    var board = manager.getNewScoreboard();
+    var objective = board.registerNewObjective("test", "dummy");
+    var score = objective.getScore("Bits: ".gray()); //Get a fake offline player
+    score.setScore(newScore);
+    //Setting where to display the scoreboard/objective (either SIDEBAR, PLAYER_LIST or BELOW_NAME)
+    objective.setDisplaySlot(DisplaySlot.SIDEBAR);
+    //Setting the display name of the scoreboard/objective
+    objective.setDisplayName("Bitcoin ".gold()+"Wallet".gray());
+    player.setScoreboard(board);
+  }
 }
